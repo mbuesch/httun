66f318612c4bdf74adb049c8f790c3946f29a22a
Michael Buesch <m@bues.ch>
2025-05-26 22:45:39 +0200

    client: Increase the number of workers

--- httun-client/src/main.rs
+++ httun-client/src/main.rs
@@ -438,7 +438,7 @@ fn main() -> ah::Result<()> {
         return Ok(());
     }
 
-    const WORKER_THREADS: usize = 4;
+    const WORKER_THREADS: usize = 6;
     runtime::Builder::new_multi_thread()
         .thread_keep_alive(Duration::from_millis(5000))
         .max_blocking_threads(WORKER_THREADS * 2)
