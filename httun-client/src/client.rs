a4db2390234d7b42e888dc9577d2b63c6c60ea71
Michael Buesch <m@bues.ch>
2025-06-12 20:38:09 +0200

    client: Fix Windows build

--- httun-client/src/client.rs
+++ httun-client/src/client.rs
@@ -28,6 +28,7 @@ use tokio::{
 
 const HTTP_R_TIMEOUT: Duration = Duration::from_secs(CHAN_R_TIMEOUT_S + 3);
 const HTTP_W_TIMEOUT: Duration = Duration::from_secs(3);
+#[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux"))]
 const TCP_USER_TIMEOUT: Duration = Duration::from_secs(2);
 const SESSION_INIT_RETRIES: usize = 5;
 
@@ -46,7 +47,10 @@ fn make_client(
     }
     c = c.referer(false);
     c = c.timeout(timeout);
-    c = c.tcp_user_timeout(TCP_USER_TIMEOUT);
+    #[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux"))]
+    {
+        c = c.tcp_user_timeout(TCP_USER_TIMEOUT);
+    }
     c = c.tcp_nodelay(true);
 
     c = c.gzip(chan_conf.http_allow_compression());
