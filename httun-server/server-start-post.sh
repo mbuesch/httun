9bc46035ee4a3fa70c070835d2638f197425ee3d
Michael Buesch <m@bues.ch>
2025-07-29 22:36:43 +0200

    server: Add examples to pre/post startup scripts

--- httun-server/server-start-post.sh
+++ httun-server/server-start-post.sh
@@ -3,6 +3,10 @@
 # httun-server - Post start script
 #
 # This script will be executed after the httun server has successfully started.
-# Put everything that has to be done after server start here:
+# Put everything that has to be done after server start here.
 
+# Abort this script on all errors.
+set -e
 
+# Assign an IP address to the TUN interface.
+#ip addr add 10.0.0.1/24 dev httun-s-a
--- httun-server/server-start-pre.sh
+++ httun-server/server-start-pre.sh
@@ -3,6 +3,9 @@
 # httun-server - Pre start script
 #
 # This script will be executed before the httun server is started.
-# Put everything that has to be done before server start here:
+# Put everything that has to be done before server start here.
+
+# Abort this script on all errors.
+set -e
 
 
