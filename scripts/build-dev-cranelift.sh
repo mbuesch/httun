4370947ffef014504bbd228eb8adc77fb89f2f71
Michael Büsch <m@bues.ch>
2025-11-27 18:49:58 +0100

    cranelift: Add prefix

--- scripts/build-dev-cranelift.sh
+++ scripts/build-dev-cranelift.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export HTTUN_CONF_PREFIX="/opt/httun"
 export CARGO_PROFILE_DEV_DEBUG=line-tables-only
 export CARGO_PROFILE_DEV_CODEGEN_BACKEND=cranelift
 exec cargo +nightly build -Zcodegen-backend
