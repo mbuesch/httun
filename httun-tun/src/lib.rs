0384003189a09e66e41c0032a5ac7b39421b46e3
Michael Büsch <m@bues.ch>
2025-12-14 10:45:35 +0100

    tun: Add doc comments

--- httun-tun/src/lib.rs
+++ httun-tun/src/lib.rs
@@ -7,14 +7,20 @@
 use anyhow::{self as ah, Context as _};
 use tokio_tun::Tun;
 
+/// Maximum transmission unit (MTU) size for the TUN interface.
 const MTU: i32 = 1024 * 62;
+/// Receive buffer size for the TUN interface.
 const RX_BUF_SIZE: usize = 1024 * 64;
 
+/// Low level TUN interface abstraction.
 pub struct TunHandler {
     tun: Tun,
 }
 
 impl TunHandler {
+    /// Create a new TUN interface.
+    ///
+    /// The `name` parameter specifies the name of the TUN interface (e.g., "tun0").
     pub async fn new(name: &str) -> ah::Result<Self> {
         let tun: Tun = Tun::builder()
             .name(name)
@@ -31,11 +37,13 @@ impl TunHandler {
         Ok(Self { tun })
     }
 
+    /// Send a packet to the TUN interface.
     pub async fn send(&self, buf: &[u8]) -> ah::Result<()> {
         self.tun.send_all(buf).await?;
         Ok(())
     }
 
+    /// Receive a packet from the TUN interface.
     pub async fn recv(&self) -> ah::Result<Vec<u8>> {
         let mut data = vec![0; RX_BUF_SIZE];
         let count = self.tun.recv(&mut data).await?;
