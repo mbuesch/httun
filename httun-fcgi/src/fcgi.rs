bb263f7228b72b8a905aff97b115a335128d7bf7
Michael Büsch <m@bues.ch>
2025-12-22 16:02:24 +0100

    Fix non-linux compile

--- httun-fcgi/src/fcgi.rs
+++ httun-fcgi/src/fcgi.rs
@@ -21,10 +21,15 @@ const INET46: [Option<libc::c_int>; 2] = [Some(libc::AF_INET), Some(libc::AF_INE
 
 /// Check if the passed raw `fd` is a socket.
 fn is_socket(fd: RawFd) -> bool {
+    #[cfg(any(target_os = "linux", target_os = "android"))]
+    use libc::{stat64, fstat64};
+    #[cfg(not(any(target_os = "linux", target_os = "android")))]
+    use libc::{stat as stat64, fstat as fstat64};
+
     // SAFETY: Initializing `libc::stat64` structure with zero is an allowed pattern.
-    let mut stat: libc::stat64 = unsafe { std::mem::zeroed() };
+    let mut stat: stat64 = unsafe { std::mem::zeroed() };
     // SAFETY: The `fd` is valid and `stat` is initialized and valid.
-    let ret = unsafe { libc::fstat64(fd, &mut stat) };
+    let ret = unsafe { fstat64(fd, &mut stat) };
     if ret == 0 {
         const S_IFMT: libc::mode_t = libc::S_IFMT as libc::mode_t;
         const S_IFSOCK: libc::mode_t = libc::S_IFSOCK as libc::mode_t;
